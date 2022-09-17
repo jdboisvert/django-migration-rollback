bd7a069c2f782dcf1d3f3419daa3cca0cd642c97
jdboisvert <info.jeffreyboisvert@gmail.com>
2022-09-16 20:24:37 -0400

    Major change to command names to instead use migrateprevious and migraterollback to follow the more traditional Django command format. Also opted for positional arguments for the same reason. Also used the internal managegement to call migrate in order to correctly show the command being run. Updated README.md to reflect this and setup.py to have correct values when publishing to pypi.

--- setup.py
+++ setup.py
@@ -1,3 +1,48 @@
-from setuptools import setup
+from os.path import dirname, join
+from pathlib import Path
 
-setup()
+from setuptools import find_packages, setup
+
+GITHUB_URL = "https://github.com/jdboisvert/django-migration-rollback"
+VERSION = "0.1.2"
+
+
+def read(fname):
+    """Read content of a file and return as a string."""
+    return Path(join(dirname(__file__), fname)).read_text()
+
+
+def get_requirements():
+    """Return requirements with loose version restrictions."""
+    return read("requirements.txt").replace("==", ">=").split("\n")
+
+
+setup(
+    name="django-migration-rollback",
+    version=VERSION,
+    license="MIT License",
+    url=GITHUB_URL,
+    download_url=f"{GITHUB_URL}/archive/refs/tags/v{VERSION}.zip",
+    project_urls={"Repository": GITHUB_URL, "Bug Reports": f"{GITHUB_URL}/issues"},
+    author="""Jeffrey Boisvert""",
+    author_email="info.jeffreyboisvert@gmail.com",
+    description="""A simple Django app to be able to migrate back to a previous migration or to migrate back to a migration in a specific branch in a git repository or locally.""",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    platforms="any",
+    install_requires=get_requirements(),
+    keywords=["django", "migration", "rollback", "git"],
+    python_requires=">=3.8.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
